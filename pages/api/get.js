af30b2b57479c0504af8fbc9578904d2eff79176
Vercel Team <pribadiarep@gmail.com>
2025-07-22 21:10:45 +0700

    Update get.js

--- pages/api/get.js
+++ pages/api/get.js
@@ -1,11 +1,18 @@
-import fs from 'fs'
-import path from 'path'
-
-export default function handler(req, res) {
+export default async function handler(req, res) {
   const { slug } = req.query
-  const filePath = path.resolve('./database/view', slug)
-  if (!fs.existsSync(filePath)) return res.status(404).json({ success: false, message: 'Not found' })
+  const filename = `${slug}.txt`
 
-  const code = fs.readFileSync(filePath, 'utf-8')
+  const githubApi = `https://api.github.com/repos/${process.env.GITHUB_USERNAME}/${process.env.GITHUB_REPO}/contents/codes/${filename}`
+
+  const response = await fetch(githubApi, {
+    headers: {
+      Authorization: `Bearer ${process.env.GITHUB_TOKEN}`
+    }
+  })
+
+  if (!response.ok) return res.status(404).json({ success: false, message: 'File not found' })
+
+  const data = await response.json()
+  const code = Buffer.from(data.content, 'base64').toString()
   res.json({ success: true, code })
 }
